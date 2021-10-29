3a3fdc7a0fb48cf5b6f0a8a89019595996aa18f9
JUB_Yoush <yoush1337@gmail.com>
2021-10-29 12:55:33 -0400

    first commit

---
+++ src/player/Player.gd
@@ -0,0 +1,19 @@
+extends Area2D
+
+var tile_size = 32
+var inputs = {
+	'right':Vector2.RIGHT,
+	'left':Vector2.LEFT,
+	'down':Vector2.DOWN,
+	'up':Vector2.UP
+}
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta: float) -> void:
+#	pass
